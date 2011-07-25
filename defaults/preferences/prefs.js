0ff8b4cce5ed7b1386e707a1468165750a6b6cea
Ed <ejnovak@email.wm.edu>
2011-07-25 14:53:20 -0400

    All these other files! :P

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,4 @@
+pref("extensions.cnt.newURL", "www.google.com");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.ejnovak@email.wm.edu.description", "chrome://cnt/locale/overlay.properties");
