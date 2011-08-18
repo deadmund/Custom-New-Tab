508369dcda29aa5016b33c16d308879e15e5a031
ed <ed@beethoven>
2011-08-17 22:16:37 -0400

    It took all day but the URL is highlighted correctly now! \o/

--- chrome/content/customNewTab.js
+++ chrome/content/customNewTab.js
@@ -1,16 +1,42 @@
-// newTab object variable thing
+
+// newTab object (for the good namespaces)
 var customNewTab = {
   onLoad : function (){
-    this.initialized = true
     gBrowser.tabContainer.addEventListener('TabOpen', customNewTab.swap, false)
   }, // End of onLoad
   
   swap : function(event){
-    var browser = gBrowser.getBrowserForTab(event.target)
-    var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch('extensions.custom-new-tab.')
-    var url = prefs.getCharPref('newURL')
-    browser.loadURI(url)
-  } // End of swap function
+    // New tabs that are caused from the green plus or control + T 
+    // and are this way.  Best way to catch I could find.
+    if ( event.explicitOriginalTarget.id == "" ){
+      dump("This is a tab I want!\n")
+    
+      var browser = gBrowser.getBrowserForTab(event.target)
+      var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch('extensions.custom-new-tab.')
+      var url = prefs.getCharPref('newURL')
+      //getAttributes(event.originalTarget)
+    
+      // This is not a good flag for how the tab was opened.
+      // All new tabs start as 'about:blank' then they get some
+      // content loaded in the and the URL changes. (not in that order)
+      //dump("contentDoc URL: " + browser.contentDocument.URL + "\n")
+    
+      browser.addEventListener('load', customNewTab.highlight, true)
+      browser.loadURI(url)
+    }
+  }, // End of swap function
+  
+  
+  // Highlights the URL (we have to wait for the content to load first
+  highlight : function(event){
+    
+    // This works because I look at the explicitOriginalTarget
+    // of the TabOpen event in the previous function
+    var bar = window.document.getElementById('urlbar')
+    bar.select()
+    browser.removeEventListener('load', customNewTab.highlight, true)
+    
+  } // End of highlight function
 }
 
 window.addEventListener('load', customNewTab.onLoad, false)
