31c4aac21696e1c42d6a74710ce9dcdb8e1d2fc2
ed <ed@beethoven>
2011-08-21 14:33:47 -0400

    Subsequent URLs in a new tab are not highlighted

--- chrome/content/customNewTab.js
+++ chrome/content/customNewTab.js
@@ -7,11 +7,11 @@ var customNewTab = {
   
   swap : function(event){
     // New tabs that are caused from the green plus or control + T 
-    // and are this way.  Best way to catch I could find.
+    // are this way.  Best way to catch I could find.
     if ( event.explicitOriginalTarget.id == "" ){
       //dump("This is a tab I want!\n")
-    
-      var browser = gBrowser.getBrowserForTab(event.target)
+      
+      customNewTab.browser = gBrowser.getBrowserForTab(event.target)
       var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch('extensions.custom-new-tab.')
       var url = prefs.getCharPref('newURL')
       //getAttributes(event.originalTarget)
@@ -19,10 +19,10 @@ var customNewTab = {
       // This is not a good flag for how the tab was opened.
       // All new tabs start as 'about:blank' then they get some
       // content loaded in the and the URL changes. (not in that order)
-      //dump("contentDoc URL: " + browser.contentDocument.URL + "\n")
+      //dump("contentDoc URL: " + customNewTab.browser.contentDocument.URL + "\n")
     
-      browser.addEventListener('load', customNewTab.highlight, true)
-      browser.loadURI(url)
+      customNewTab.browser.addEventListener('load', customNewTab.highlight, true)
+      customNewTab.browser.loadURI(url)
     }
   }, // End of swap function
   
@@ -34,8 +34,8 @@ var customNewTab = {
     // of the TabOpen event in the previous function
     var bar = window.document.getElementById('urlbar')
     bar.select()
-    browser.removeEventListener('load', customNewTab.highlight, true)
-    
+    customNewTab.browser.removeEventListener('load', customNewTab.highlight, true)
+
   } // End of highlight function
 }
 
