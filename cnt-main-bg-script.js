eea5d7b84eb9349eaee8fe7d8c98c9d643eafb43
Ed <ejnovak@email.wm.edu>
2016-06-04 10:05:53 -0400

    Put in code to catch browser.newtab.preload = true I think that there was a bug in previous version of FF (<46) that has now been fixed.

--- cnt-main-bg-script.js
+++ cnt-main-bg-script.js
@@ -3,35 +3,48 @@ console.log("Some output from cnt-main-bg-script!");
 
 // for debug purposes
 function dumpKeys(obj){
+	console.log("Dumping: " + obj)
 	for(var k in obj){
 		console.log(k + " " + obj[k]);
 	}
 }
 
+function redir(tab){
+	// Redirect tab to user preference.  This method leaves focus in URL bar
+	browser.storage.local.get("url_pref", function(url_data){
+		browser.storage.local.get("focus_pref", function(focus_data){
+			if(focus_data.focus_pref == "focus_bar"){
+				// This method leaves the focus in the URL bar
+				browser.tabs.update(tab.id, {"url":url_data.url_pref});
+			} 
+
+			else if (focus_data.focus_pref == "focus_page"){
+				// This workaround puts the focus on the page (cause of the call to create) instead of in the URL bar
+				chrome.tabs.remove(tab.id); // close the new tab that was opened
+				chrome.tabs.create({"url":url_data.url_pref, "active":true}); // open our own tab
+			}
+		});
+	});
+
+}
+
 function newTab(newTab){
+	console.log("New tab has been opened, before it has finished loading here is the url: " + newTab.tab.url + "  and here is the status: " + newTab.tab.status)
+	//dumpKeys(newTab.tab)
+
+	// If browser.newtab.preload is true, this probably occurs, because the tab has already been loaded
+	if(newTab.tab.status === "complete" && newTab.tab.url == "about:newtab"){
+		redir(newTab.tab)
+	}
+
+	// If preload is false, we have to wait for it to load, then we can redirect
 	browser.tabs.onUpdated.addListener(function(tabID, info, tab){
 		if(info.status == "complete"){
 			// After tab has finished loading remove listener
 			browser.tabs.onUpdated.removeListener(arguments.callee);
 
 			if(tab.url == "about:newtab"){
-
-				// Redirect tab to user preference.  This method leaves focus in URL bar
-				browser.storage.local.get("url_pref", function(url_data){
-					browser.storage.local.get("focus_pref", function(focus_data){
-						if(focus_data.focus_pref == "focus_bar"){
-							// This method leaves the focus in the URL bar
-							browser.tabs.update(tabID, {"url":url_data.url_pref});
-						} 
-
-						else if (focus_data.focus_pref == "focus_page"){
-							// This workaround puts the focus on the page (cause of the call to create) instead of in the URL bar
-							chrome.tabs.remove(tab.id); // close the new tab that was opened
-							chrome.tabs.create({"index":30, "url":url_data.url_pref, "active":true}); // open our own tab
-
-						}
-					});
-				});
+				redir(newTab.tab);
 			}
 		}
 	});
